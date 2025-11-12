0e29d9fea94625600f8fd83691d3349bf2d2e164
mdjoynalhosen20 <joynalhosen10@gmail.com>
2025-11-12 14:14:20 +0600

    MongoDB successfully set up

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+}); 
+
+const uri = process.env.DATABASE_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // * All Collection list are here 
+        const database = client.db("ImportExport_DB"); 
+        const usersCollection = database.collection("users"); 
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
